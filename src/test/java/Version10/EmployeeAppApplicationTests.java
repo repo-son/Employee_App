6966d284012fa1fec6002bb186b406cc68040882
repo-son <lahiru980529@gmail.com>
2021-09-03 18:55:09 +0530

    Creating Application Properties

---
+++ src/test/java/Version10/EmployeeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package Version10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
